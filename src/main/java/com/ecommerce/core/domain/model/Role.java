e1163a50b0277b786c871e19e62309e6474aeb74
teshy18 <sebas.tesitore@gmail.com>
2025-06-10 01:48:48 -0300

    Refactor: Se agregan anotaciones con lombok en la clase Role

--- src/main/java/com/ecommerce/core/domain/model/Role.java
+++ src/main/java/com/ecommerce/core/domain/model/Role.java
@@ -1,7 +1,19 @@
 package com.ecommerce.core.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     
     private int id ; 
-    private String name; 
+    private String name;
+     
 }
