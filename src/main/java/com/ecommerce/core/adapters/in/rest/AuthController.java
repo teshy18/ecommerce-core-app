6fb3a06042b07215e03ffcd88a44e9deefd63d91
teshy18 <sebas.tesitore@gmail.com>
2025-06-09 23:20:41 -0300

    Implementa el controlador de autenticación con métodos de registro y inicio de sesión

---
+++ src/main/java/com/ecommerce/core/adapters/in/rest/AuthController.java
@@ -0,0 +1,27 @@
+package com.ecommerce.core.adapters.in.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    //private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
